7016c5db6df7d34ab211dbe58d5fdbb524d6e8d7
Dmitriy <romashka14108@mail.ru>
2022-08-01 12:44:32 +0300

    add calculator and game

---
+++ Calculator.java
@@ -0,0 +1,28 @@
+public  class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Enter first operand:");
+        int a = 2;
+        System.out.println("Enter operation:");
+        char mathOperation = '^';
+        System.out.println("Enter second operand:");
+        int b = 3;
+        double result = 0;
+        if (mathOperation == '+') {
+        result = a + b;
+        } else if (mathOperation == '-') {
+        result = a - b;
+        } else if (mathOperation == '*') {
+        result = a * b;
+        } else if (mathOperation == '/') {
+        result = a / b;
+        } else if (mathOperation == '%') {
+        result = a % b;
+        } else if (mathOperation == '^') {
+            result = 1;
+            for (int i = 0; i < b; i++) {
+                result *= a;
+            }
+        }
+        System.out.println("Result = " + result);
+    }
+}
