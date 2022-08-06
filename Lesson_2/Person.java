7173206e9de4fd529b7f670efc034922d2bc787a
Dmitriy <romashka14108@mail.ru>
2022-08-06 16:01:43 +0300

    add homework Class

---
+++ Lesson_2/Person.java
@@ -0,0 +1,29 @@
+package Lesson_2;
+
+public class Person {
+    String sex = "Male";
+    String name = "Dmitriy";
+    int height = 179;
+    int weight = 96;
+    int age = 43;
+
+    void go() {
+        System.out.println("Goes");
+    }
+
+    void sit() {
+        System.out.println("Sits");
+    }
+
+    void run() {
+        System.out.println("Running");
+    }
+
+    void speak() {
+        System.out.println("Says");
+    }
+
+    void learn() {
+        System.out.println("Learn Java");
+    }
+}
